c526bdbe3b9d8029bc1bd734e9235d9bbf5ddbdb
Timo <developer.timo@gmail.com>
2022-02-06 09:09:12 +0900

    [스프링 핵심 원리 이해2 - 객체 지향 원리 적용] 관심사의 분리 - OrderService 객체 생성, 의존성 주입을 AppConfig 클래스에게 위임 - SRP, DIP 원칙을 지키도록 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,8 +1,11 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
 /**
  * 객체를 생성, 연결하는 책임을 갖는 클래스
@@ -14,6 +17,11 @@ public class AppConfig {
      */
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        //서비스에서 사용할 레포지토리와 할인 정책의 구현체를 지정한다.
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 
 }
