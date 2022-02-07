7e0734ee275d28c0cd4b016a624fb6d1d10e238a
Timo <developer.timo@gmail.com>
2022-02-07 17:21:24 +0900

    [스프링 핵심 원리 이해2 - 객체 지향 원리 적용] 스프링으로 전환하기 - 스프링 컨테이너에 등록하기 위한 작업 - @Configuration 어노테이션으로 구성 클래스임을 표현 - @Bean 어노테이션으로 각 객체들을 스프링 빈으로 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,29 +8,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 객체를 생성, 연결하는 책임을 갖는 클래스
  */
+@Configuration
 public class AppConfig {
 
+    //@Bean 어노테이션을 통해 스프링 빈으로 등록해준다.
+    @Bean
     public MemberService memberService() {
-        //레포지토리 구현체를 MemoryMemberRepository로 지정
         return new MemberServiceImpl(memberRepository());
     }
 
-    //레포지토리의 구현체를 지정한다.
+    @Bean
     public MemberRepository memberRepository() {
+        //레포지토리의 구현체를 지정한다.
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    //할인 정책의 구현체를 지정한다.
-    private DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //할인 정책의 구현체를 지정한다.
+        //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
