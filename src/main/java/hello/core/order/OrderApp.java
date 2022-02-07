4d95ad0069831957c3241b47ea4aded27c248080
Timo <developer.timo@gmail.com>
2022-02-07 17:27:56 +0900

    [스프링 핵심 원리 이해2 - 객체 지향 원리 적용] 스프링으로 전환하기 - 스프링 컨테이너를 통해 의존 관계 관리

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -4,13 +4,17 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //구성 정보 클래스를 바탕으로 스프링 컨테이너 호출
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //스프링 컨테이너에 등록되어 있는 스프링 빈 호출
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
