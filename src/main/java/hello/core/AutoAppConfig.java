004aab19c14ceb72a8aafbc3a2cd5f479faf9810
Timo <developer.timo@gmail.com>
2022-02-09 21:51:43 +0900

    [컴포넌트 스캔] 탐색 위치와 기본 스캔 대상 - @ComponentScan 어노테이션에 basePackages 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-//기존 AppConfig 클래스는 등록되지 않도록 제외
 @ComponentScan(
+        //basePackages: 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.
+        //지정하지 않으면 @ComponentScan 어노테이션이 붙은 설정 정보의 패키지가 시작 위치가 된다.
+        basePackages = "hello.core",
+        //기존 AppConfig 클래스는 등록되지 않도록 제외
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
 
 }
