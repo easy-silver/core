df43182e9911560c1fac2bf0b08178984aceed2b
Timo <developer.timo@gmail.com>
2022-02-09 16:49:39 +0900

    [싱글톤 컨테이너] @Configuration과 싱글톤 - 스프링 빈 생성 과정을 확인하기 위한 호출 로그 추가

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -20,17 +20,20 @@ public class AppConfig {
     //@Bean 어노테이션을 통해 스프링 빈으로 등록해준다.
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         //레포지토리의 구현체를 지정한다.
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
